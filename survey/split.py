08de00fe11b01357e6561510fb2d8751d4d658eb
Alexandre Detiste <alexandre.detiste@gmail.com>
2022-10-23 17:25:47 +0200

    process 100%

--- survey/split.py
+++ survey/split.py
@@ -23,27 +23,40 @@ for postrm in glob.glob('unstable/*/postrm'):
 
     with open(postrm, 'r') as concatenated:
          for line in concatenated:
-             line = line.strip('\n').rstrip(' ')
+             line = line.strip('\n').rstrip(' \t')
 
              # unstable/spamass-milter/postrm
              while(line.startswith('##')):
                  line = line[1:]
 
              # unstable/ganglia-monitor/postrm
-             if line and line[0] != '#' and line.lstrip(' ')[0] == '#':
-                 line = line.lstrip(' ')
+             if line and line[0] != '#' and line.lstrip(' \t')[0] == '#':
+                 line = line.lstrip(' \t')
 
              if line.startswith(START):
-                 helper = line[len(START):].split('/')[0]
+                 helper = line[len(START):]
                  if helper.startswith('dh_'):
-                     helper = helper[4:]
-             elif line == END and not helper:
+                     helper = helper[3:]
+
+                 if helper.startswith('debian/scripts/'):
+                     # localy provided script
+                     helper = 'private_script_%s' % package
+                 elif '/' in helper:
+                     helper, version = helper.split('/', 1)
+                     if version in ('UNDECLARED', '13.4+nmu1'):
+                         pass
+                     elif version.strip('0123456789-.'):
+                         print('Weird version: %s: %s' % (postrm, version))
+                 lines_helper.append(line)
+
+             elif line == END and helper is None:
                  print('Borked file: %s' % postrm)
              elif line == END:
                  mkdir_p(package_dir)
                  dest = os.path.join(package_dir, helper)
+                 lines_helper.append(line)
                  with open(dest, 'w') as splitted:
-                     splitted.write('\n'.join(lines_helper))
+                     splitted.write('\n'.join(lines_helper) + '\n')
                  helper = None
                  lines_helper = list()
              elif helper:
@@ -55,5 +68,7 @@ for postrm in glob.glob('unstable/*/postrm'):
         mkdir_p(package_dir)
         dest = os.path.join(package_dir, 'postrm')
         with open(dest, 'w') as splitted:
-            splitted.write('\n'.join(lines_hardcoded))
+            splitted.write('\n'.join(lines_hardcoded) + '\n')
 
+
+os.system('find split/ -type f -printf "%f\n" | sort | uniq -c')
